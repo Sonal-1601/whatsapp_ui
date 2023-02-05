3c791fee8975c49d11512a966cb3ad1b234e58e7
Sonal-1601 <pandeysonal1601@gmail.com>
2023-02-06 00:04:14 +0530

    finish contact page

--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -1,33 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/colors.dart';
+import 'package:whatsapp_ui/info.dart';
 
-import '../info.dart';
+import '../screens/mobile_chat_screen.dart';
 
 class ContactsList extends StatelessWidget {
-  const ContactsList({super.key});
+  const ContactsList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 10),
+      padding: const EdgeInsets.only(top: 8.0),
       child: ListView.builder(
-          itemCount: info.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(
-                info[index]['name'].toString(),
-                style: const TextStyle(
-                  fontSize: 18,
+        shrinkWrap: true,
+        itemCount: info.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const MobileChatScreen(),
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 4.0),
+                  child: ListTile(
+                    title: Text(
+                      info[index]['name'].toString(),
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 6.0),
+                      child: Text(
+                        info[index]['message'].toString(),
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                    ),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        info[index]['profilePic'].toString(),
+                      ),
+                      radius: 30,
+                    ),
+                    trailing: Text(
+                      info[index]['time'].toString(),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  info[index]['message'].toString(),
-                  style: const TextStyle(fontSize: 15),
-                ),
-              ),
-            );
-          }),
+              // const Divider(color: dividerColor, indent: 95),
+            ],
+          );
+        },
+      ),
     );
   }
 }
