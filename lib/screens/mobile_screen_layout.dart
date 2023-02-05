f5ccf81a86cd2cdbaae0381fca89705b5c65b64a
Sonal-1601 <pandeysonal1601@gmail.com>
2023-02-05 23:11:49 +0530

    add appBar and theme

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:whatsapp_ui/colors.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
@@ -12,6 +13,40 @@ class MobileScreenLayout extends StatefulWidget {
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("this is mobile")));
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: appBarColor,
+            elevation: 0,
+            title: const Text(
+              "WhatsApp",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            centerTitle: false,
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.camera_alt_outlined)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+            ],
+            bottom: const TabBar(
+                indicatorColor: tabColor,
+                indicatorWeight: 4,
+                labelColor: tabColor,
+                unselectedLabelColor: Colors.grey,
+                labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                tabs: [
+                  Tab(text: "Chats"),
+                  Tab(text: "Status"),
+                  Tab(text: "Calls"),
+                ]),
+          ),
+          body: Text('mobile'),
+        ));
   }
 }
